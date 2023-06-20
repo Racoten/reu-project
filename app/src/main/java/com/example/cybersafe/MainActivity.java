4c8daaa60115ac05cc067ccdf9422686786ce3c4
Gareth M <gkm25@protonmail.com>
2023-06-20 16:29:55 -0500

    started login and signup activities

--- app/src/main/java/com/example/cybersafe/MainActivity.java
+++ app/src/main/java/com/example/cybersafe/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cybersafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginPage(View view) {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void signupPage(View view) {
+        Intent signupIntent = new Intent(this, SignupActivity.class);
+        startActivity(signupIntent);
     }
 }
