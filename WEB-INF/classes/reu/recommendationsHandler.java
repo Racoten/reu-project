c39842422aebb264187633ab7102ea2f5472075d
iSuperstar <40224197+iSuperstar@users.noreply.github.com>
2023-06-23 19:25:34 -0500

    Done with questions and recommendations

--- WEB-INF/classes/reu/recommendationsHandler.java
+++ WEB-INF/classes/reu/recommendationsHandler.java
@@ -7,13 +7,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.*;
 
-public class questionsHandler extends HttpServlet {
+public class recommendationsHandler extends HttpServlet {
     public MySQLConnector myDBConn;
 
-    ArrayList<String> questions = new ArrayList<String>();
+    ArrayList<String> recommendations = new ArrayList<String>();
     public String answer;
 
-	public questionsHandler(){
+	public recommendationsHandler(){
 		//Create the MySQLConnector object
 		myDBConn = new MySQLConnector();
 		
@@ -29,7 +29,23 @@ public class questionsHandler extends HttpServlet {
 		System.out.println("Receive request with parameter: " + param);
 
         if(param.equals("emailgeneral")) {
-			ArrayList<String> questions = getGeneralQuestions("emailquestions");
+			ArrayList<String> questions = getGeneralRecommendations("emailrecommendations");
+			String msg = "{";
+
+			for(int i = 0; i < questions.size(); i++) {
+				msg += "\"" + (i+1) + "\": \"" + questions.get(i) + "\"";
+				if(i < questions.size()-1) {
+					msg += ",";
+				}
+			}
+
+			msg += "}";
+			PrintWriter out = response.getWriter();
+			out.println(msg);
+		} 
+
+        else if(param.equals("browsersecuritygeneral")) {
+			ArrayList<String> questions = getGeneralRecommendations("browsersecurityrecommendations");
 			String msg = "{";
 
 			for(int i = 0; i < questions.size(); i++) {
@@ -44,8 +60,8 @@ public class questionsHandler extends HttpServlet {
 			out.println(msg);
 		}
 
-		else if(param.equals("browsersecuritygeneral")) {
-			ArrayList<String> questions = getGeneralQuestions("browsersecurityquestions");
+        else if(param.equals("emailtargeted")) {
+			ArrayList<String> questions = getTargetedRecommendations("emailrecommendationstargeted", targetedTable);
 			String msg = "{";
 
 			for(int i = 0; i < questions.size(); i++) {
@@ -59,9 +75,9 @@ public class questionsHandler extends HttpServlet {
 			PrintWriter out = response.getWriter();
 			out.println(msg);
 		} 
-        
-        else if(param.equals("browsertargeted")) {
-            ArrayList<String> questions = getTargetedQuestions("browsersecurityquestionstargeted", targetedTable);
+
+        else if(param.equals("browsersecuritytargeted")) {
+			ArrayList<String> questions = getTargetedRecommendations("browsersecurityrecommendationstargeted", targetedTable);
 			String msg = "{";
 
 			for(int i = 0; i < questions.size(); i++) {
@@ -74,73 +90,44 @@ public class questionsHandler extends HttpServlet {
 			msg += "}";
 			PrintWriter out = response.getWriter();
 			out.println(msg);
-        } 
-        
-        else if(param.equals("emailtargeted")) {
-            ArrayList<String> questions = getTargetedQuestions("emailquestionstargeted", targetedTable);
-			String msg = "{";
-
-			for(int i = 0; i < questions.size(); i++) {
-				msg += "\"" + (i+1) + "\": \"" + questions.get(i) + "\"";
-				if(i < questions.size()-1) {
-					msg += ",";
-				}
-			}
-
-			msg += "}";
-			PrintWriter out = response.getWriter();
-			out.println(msg);
-        }
-
-        else {
-            // Set response content type
-            response.setContentType("text/html");
-
-            // Actual logic goes here.
-            PrintWriter out = response.getWriter();
-            
-            // Send the response
-            out.println("There was an issue with the parameter...");
-        }
+		}
 	 }
 
-    public ArrayList<String> getGeneralQuestions(String type) {
-        String query = "SELECT QuestionText FROM "+type+";";
+     public ArrayList<String> getGeneralRecommendations(String type) {
+        String query = "SELECT RecommendationText FROM " + type + ";";
         ResultSet querySend = myDBConn.doSelect(query);
-        
-        if (!questions.isEmpty()) {
-            questions.clear();
-        }
 
+        if (!recommendations.isEmpty()) {
+            recommendations.clear();
+        }
         try {
-            while (querySend.next()) {
-                questions.add(querySend.getString("QuestionText"));
-                System.out.println("Question being added: " + querySend.getString("QuestionText"));
+            while(querySend.next()) {
+                recommendations.add(querySend.getString("RecommendationText"));
+                System.out.println("Recommendation being added: " + querySend.getString("RecommendationText"));
             }
         } catch (Exception e) {
             System.out.println(e);
-        } finally { 
-            return questions;
+        } finally {
+            return recommendations;
         }
-    }
+     }
 
-    public ArrayList<String> getTargetedQuestions(String type, String table) {
-        String query = "SELECT QuestionText FROM " + type + table + ";";
+     public ArrayList<String> getTargetedRecommendations(String type, String table) {
+        String query = "SELECT RecommendationText FROM " + type  + table + ";";
         ResultSet querySend = myDBConn.doSelect(query);
-        
-        if (!questions.isEmpty()) {
-            questions.clear();
-        }
 
+        if (!recommendations.isEmpty()) {
+            recommendations.clear();
+        }
         try {
-            while (querySend.next()) {
-                questions.add(querySend.getString("QuestionText"));
-                System.out.println("Question being added: " + querySend.getString("QuestionText"));
+            while(querySend.next()) {
+                recommendations.add(querySend.getString("RecommendationText"));
+                System.out.println("Recommendation being added: " + querySend.getString("RecommendationText"));
             }
         } catch (Exception e) {
             System.out.println(e);
-        } finally { 
-            return questions;
+        } finally {
+            return recommendations;
         }
-    }
+     }
 }
